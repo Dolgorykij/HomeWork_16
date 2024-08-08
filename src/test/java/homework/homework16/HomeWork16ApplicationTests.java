213b8ce1f3922290dfbf2b4c601dd2cfbfd8f6df
Dolgorykij <dolgo-rykij@mail.ru>
2024-08-09 01:36:05 +0300

    ini

---
+++ src/test/java/homework/homework16/HomeWork16ApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.homework16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
